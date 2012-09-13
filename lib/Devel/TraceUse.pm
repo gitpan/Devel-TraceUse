0fb09d6a740bacc332960eec53c7e008857a26c9
Philippe Bruhat (BooK) <book@cpan.org>
2012-09-13 11:02:54 +0000

    Import of BOOK/Devel-TraceUse-2.08 from CPAN.
    gitpan-cpan-distribution: Devel-TraceUse
    gitpan-cpan-version:      2.08
    gitpan-cpan-path:         BOOK/Devel-TraceUse-2.08.tar.gz
    gitpan-cpan-author:       BOOK
    gitpan-cpan-maturity:     released

--- lib/Devel/TraceUse.pm
+++ lib/Devel/TraceUse.pm
@@ -6,7 +6,7 @@ sub DB {}
 
 package Devel::TraceUse;
 
-our $VERSION = '2.07';
+our $VERSION = '2.08';
 
 BEGIN
 {
@@ -334,9 +334,9 @@ Philippe Bruhat, C<< <book at cpan.org> >>
 
 C<hidecore> option contributed by David Leadbeater, C<< <dgl@dgl.cx> >>.
 
-C<output> option contributed by Olivier MenguÃ© (C<< <dolmen@cpan.org> >>).
+C<output> option contributed by Olivier Mengué (C<< <dolmen@cpan.org> >>).
 
-C<perl -c> support contributed by Olivier MenguÃ© (C<< <dolmen@cpan.org> >>).
+C<perl -c> support contributed by Olivier Mengué (C<< <dolmen@cpan.org> >>).
 
 =head1 BUGS
 
